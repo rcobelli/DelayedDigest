1db93a1c669976337204f5596d7dd0f264e59675
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2021-01-05 16:04:58 -0500

    Use new debug cookie

--- init.php
+++ init.php
@@ -8,8 +8,13 @@ require 'PlancakeEmailParser.php';
 // Set the timezone
 date_default_timezone_set('America/New_York');
 
-// Hide errors
-error_reporting(0);
+if ($_COOKIE['debug'] == 'true') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(-1);
+} else {
+    error_reporting(0);
+}
 
 use Aws\S3\S3Client;
 
